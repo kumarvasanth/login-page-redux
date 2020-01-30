df16df883e65ae786659291a741eb0dd28875026
Vasanth kaumar <vasanthvasu062@gmail.com>
2020-01-30 23:21:09 +0530

    udates

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -5,29 +5,31 @@ import "../App.css";
 class Employee extends React.Component {
   render() {
     return (
-      <h5>Displaying Json data in Table format </h5>
-      <table className="tabl">
-        <tr>
-          <th>Id</th>
-          <th>Name</th>
-          <th>Age</th>
-          <th>Gender</th>
-          <th>Email</th>
-          <th>PhoneNumber</th>
-        </tr>
-        {this.props.employee.map(emp => {
-          return (
-            <tr key={emp.id}>
-              <td>{emp.id}</td>
-              <td>{emp.name}</td>
-              <td>{emp.age}</td>
-              <td>{emp.gender}</td>
-              <td>{emp.email}</td>
-              <td>{emp.phoneNo}</td>
-            </tr>
-          );
-        })}
-      </table>
+      <React.Fragment>
+        <h5>Displaying Json data in Table format </h5>
+        <table className="tabl">
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Gender</th>
+            <th>Email</th>
+            <th>PhoneNumber</th>
+          </tr>
+          {this.props.employee.map(emp => {
+            return (
+              <tr key={emp.id}>
+                <td>{emp.id}</td>
+                <td>{emp.name}</td>
+                <td>{emp.age}</td>
+                <td>{emp.gender}</td>
+                <td>{emp.email}</td>
+                <td>{emp.phoneNo}</td>
+              </tr>
+            );
+          })}
+        </table>
+      </React.Fragment>
     );
   }
 }
